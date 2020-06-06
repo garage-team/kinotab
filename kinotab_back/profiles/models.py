17eeb6255024cf6d3c8735a6df2b0f39be08b4f1
byteturner <dmtokwd@gmail.com>
2020-06-06 14:13:55 +0300

    required user field added

--- kinotab_back/profiles/models.py
+++ kinotab_back/profiles/models.py
@@ -1,14 +1,18 @@
-from django.contrib.auth.models import (AbstractBaseUser)
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     """User model use email instead username"""
-    username = None
     email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     objects = CustomUserManager()
 
