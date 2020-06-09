397c42028dbd15a92cbdcf2faa05dab30797bc76
byteturner <dmtokwd@gmail.com>
2020-06-09 13:19:42 +0300

    apps collection and movie are added

---
+++ kinotab_back/collection/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from movie.models import movie
+from django.conf import settings
+
+class tag(models.Model):
+    name = models.CharField(max_length=256)
+
+class user_state(models.Model):
+    watched = models.BooleanField()
+    score = models.IntegerField(null=True, blank=True)
+
+class user_entry(models.Model):
+    movie = models.ForeignKey(movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tag)
+    state = models.ForeignKey(user_state, on_delete=models.CASCADE)
