6cea24b53c8af589190a5dc176acd12a8e7bbca8
Ульянцев <aleksandr.uliantsev@playrix.com>
2020-06-13 02:51:37 +0300

    fix use a user signal

--- kinotab_back/profiles/signals.py
+++ kinotab_back/profiles/signals.py
@@ -1,16 +1,11 @@
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Profile
+from profiles.models import User, Profile
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **_):
     if created:
         Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **_):
     instance.profile.save()
